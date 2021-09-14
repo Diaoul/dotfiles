1ad6ec0ec13ca811e2256425770de89d4e70fe2a
Antoine Bertin <antoine@bertin.io>
2021-09-14 17:56:28 +0200

    docs: add comment to saver.sh

--- .local/bin/saver.sh
+++ .local/bin/saver.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# to get the geometry of the windows and select the wallpaper accordingly:
+#xdotool getwindowgeometry ${XSCREENSAVER_WINDOW} | rg --only-matching --replace '$1' 'Geometry: (.*)$'
 feh --scale-down --zoom fill --window-id="${XSCREENSAVER_WINDOW}" "${XSECURELOCK_SAVER_WALLPAPER}"
