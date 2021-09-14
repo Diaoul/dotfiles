5285659be473f9a7e7e5bda82185c84f2b0df101
Antoine Bertin <antoine@bertin.io>
2021-09-14 18:00:33 +0200

    feat: polybar new setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# logs
+LOG_DIR=$HOME/.cache/polybar
+mkdir -p $LOG_DIR
+echo '---' | tee -a $LOG_DIR/main.log $LOG_DIR/secondary.log > /dev/null
+
+# kill
 killall -q polybar
 
-MONITOR=$(polybar -m | grep primary | sed -e 's/:.*$//g') polybar -q main &
+# start main polybar on primary monitor
+MONITOR=$(polybar -m | grep primary | sed -e 's/:.*$//g') \
+    polybar main &>> $LOG_DIR/main.log & \
+    disown
+
+# start secondary polybar on other monitors
 for monitor in $(polybar -m | grep -v primary | sed -e 's/:.*$//g'); do
-    MONITOR=$monitor polybar -q secondary &
+    MONITOR=$monitor \
+        polybar secondary 2>&1 | \
+        sed "s/^/$monitor: /" >> $LOG_DIR/secondary.log &
+        disown
 done
