d3f70341e6db65eb78cbdfe3d414c172b9b90dcd
Antoine Bertin <antoine@bertin.io>
2021-10-17 23:10:11 +0200

    fix(nvim): clean up indentline

--- .config/nvim/lua/diaoul/indentline.lua
+++ .config/nvim/lua/diaoul/indentline.lua
@@ -15,16 +15,6 @@ function M.config()
         'terminal',
         'nofile',
     }
-
-    -- current context
-    -- see https://github.com/lukas-reineke/indent-blankline.nvim/issues/130
-    -- vim.g.indent_blankline_show_current_context = true
-    -- vim.opt.colorcolumn = '99999'
-    -- vim.g.indent_blankline_context_patterns =
-    --     {
-    --         'class', 'function', 'method', '^if', '^while', '^for', '^object',
-    --         '^table', 'block', 'arguments'
-    --     }
 end
 
 return M
