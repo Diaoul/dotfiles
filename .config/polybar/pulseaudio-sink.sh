8cfe7186a3c163420574ec9a13e7e9472909d904
Antoine Bertin <antoine@bertin.io>
2022-07-30 22:37:30 +0200

    fix(polybar): next-node and clean unused

--- .config/polybar/pulseaudio-sink.sh
+++ .config/polybar/pulseaudio-sink.sh
@@ -2,20 +2,13 @@
 pulseaudio-control \
     --format "\$ICON_NODE \$NODE_NICKNAME" \
     --node-nicknames-from "device.description" \
-    --node-nickname "bluez_output.70_CE_8C_DA_10_81.a2dp-sink: %{F#a89984}%{F-} Galaxy Buds+" \
+    --node-blacklist "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_5__sink" \
     --node-nickname "bluez_output.14_3F_A6_10_80_39.a2dp-sink: %{F#a89984}%{F-} WH-1000XM4" \
+    --node-nickname "bluez_output.88_C9_E8_1D_2E_33.a2dp-sink: %{F#a89984}%{F-} WF-1000XM4" \
     --node-nickname "alsa_output.usb-0b0e_Jabra_Link_370_3050756D9E4B-00.analog-stereo: %{F#a89984}%{F-} Jabra" \
-    --node-nickname "alsa_output.usb-Logitech_G935_Gaming_Headset-00.analog-stereo: %{F#a89984}%{F-} Logitech G935" \
-    --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi___ucm0001.hw_sofhdadsp__sink: %{F#a89984}蓼 %{F-}Speaker" \
-    --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi___ucm0003.hw_sofhdadsp__sink: %{F#a89984}蓼 %{F-}Speaker" \
     --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink: %{F#a89984}蓼 %{F-}Speaker" \
-    --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi___ucm0003.hw_sofhdadsp_3__sink: %{F#a89984}蓼 %{F-}Output 1" \
-    --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi___ucm0001.hw_sofhdadsp_3__sink: %{F#a89984}蓼 %{F-}Output 1" \
     --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_3__sink: %{F#a89984}蓼 %{F-}Output 1" \
-    --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi___ucm0003.hw_sofhdadsp_4__sink: %{F#a89984}蓼 %{F-}Output 2" \
-    --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi___ucm0001.hw_sofhdadsp_4__sink: %{F#a89984}蓼 %{F-}Output 2" \
     --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_4__sink: %{F#a89984}蓼 %{F-}Output 2" \
-    --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi___ucm0003.hw_sofhdadsp_5__sink: %{F#a89984}蓼 %{F-}Output 3" \
-    --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi___ucm0001.hw_sofhdadsp_5__sink: %{F#a89984}蓼 %{F-}Output 3" \
     --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_5__sink: %{F#a89984}蓼 %{F-}Output 3" \
-    listen
+    $1
+
