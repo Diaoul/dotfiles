c2be158ebfa9e5c285118118c90a0a63068a8d32
Antoine Bertin <antoine@bertin.io>
2022-11-15 08:11:56 +0100

    fix(pulseaudio): sink names for icons

--- .config/polybar/pulseaudio-sink.sh
+++ .config/polybar/pulseaudio-sink.sh
@@ -1,10 +1,11 @@
-
+#!/usr/bin/env bash
+# run `pactl list short sinks` to get sink names
 pulseaudio-control \
     --format "\$ICON_NODE \$NODE_NICKNAME" \
     --node-nicknames-from "device.description" \
     --node-blacklist "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_5__sink" \
-    --node-nickname "bluez_output.14_3F_A6_10_80_39.a2dp-sink: %{F#a89984}%{F-} WH-1000XM4" \
-    --node-nickname "bluez_output.88_C9_E8_1D_2E_33.a2dp-sink: %{F#a89984}%{F-} WF-1000XM4" \
+    --node-nickname "bluez_output.14_3F_A6_10_80_39.1: %{F#a89984}%{F-} WH-1000XM4" \
+    --node-nickname "bluez_output.88_C9_E8_1D_2E_33.1: %{F#a89984}%{F-} WF-1000XM4" \
     --node-nickname "alsa_output.usb-0b0e_Jabra_Link_370_3050756D9E4B-00.analog-stereo: %{F#a89984}%{F-} Jabra" \
     --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink: %{F#a89984}蓼 %{F-}Speaker" \
     --node-nickname "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_3__sink: %{F#a89984}蓼 %{F-}Output 1" \
