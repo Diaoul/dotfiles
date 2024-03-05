59a130d7ff78090f0644ab8331dbf0fca541041f
Antoine Bertin <antoine@bertin.io>
2024-03-05 22:22:05 +0100

    feat(nvim): add colorizer

--- .config/nvim/lua/diaoul/plugins/editor.lua
+++ .config/nvim/lua/diaoul/plugins/editor.lua
@@ -249,6 +249,13 @@ return {
     },
   },
 
+  -- Colorizer
+  {
+    "NvChad/nvim-colorizer.lua",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {},
+  },
+
   -- Automatically highlights other instances of the word under your cursor.
   -- This works with LSP, Treesitter, and regexp matching to find the other
   -- instances.
