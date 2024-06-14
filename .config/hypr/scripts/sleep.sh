1bd81a52ddc6f31a6be5a68b68b853579baf58d4
Antoine Bertin <antoine@bertin.io>
2024-06-14 20:39:27 +0200

    feat: idle based on battery state

--- .config/hypr/scripts/sleep.sh
+++ .config/hypr/scripts/sleep.sh
@@ -2,9 +2,16 @@
 set -euo pipefail
 
 swayidle -w \
-  timeout 300 'swaylock -f' \
-  timeout 600 'hyprctl dispatch dpms off' \
-  timeout 900 'systemctl suspend' \
+  lock 'swaylock -f' \
+  timeout 180 'swaylock -f' \
+  timeout 120 '[ "$(cat /sys/class/power_supply/AC/online)" = "0" ] && brightnessctl -d intel_backlight -c backlight -s && brightnessctl -d intel_backlight -c backlight set 5%' \
+  resume 'brightnessctl -d intel_backlight -c backlight -r' \
+  timeout 300 '[ "$(cat /sys/class/power_supply/AC/online)" = "0" ] && hyprctl dispatch dpms off' \
   resume 'hyprctl dispatch dpms on' \
-  before-sleep 'swaylock -f' \
-  lock 'swaylock -f'
+  timeout 600 '[ "$(cat /sys/class/power_supply/AC/online)" = "0" ] && systemctl suspend' \
+  resume 'hyprctl dispatch dpms on' \
+  timeout 600 '[ "$(cat /sys/class/power_supply/AC/online)" = "1" ] && hyprctl dispatch dpms off' \
+  resume 'hyprctl dispatch dpms on' \
+  timeout 1200 '[ "$(cat /sys/class/power_supply/AC/online)" = "1" ] && systemctl suspend' \
+  resume 'hyprctl dispatch dpms on' \
+  before-sleep 'swaylock -f'
