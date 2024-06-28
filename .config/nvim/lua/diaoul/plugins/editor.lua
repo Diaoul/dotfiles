a2f14fa69af04f1dd218ebca353791eb87b22f50
Antoine Bertin <antoine@bertin.io>
2024-06-28 10:06:44 +0200

    fix(nvim): neotree file view

--- .config/nvim/lua/diaoul/plugins/editor.lua
+++ .config/nvim/lua/diaoul/plugins/editor.lua
@@ -33,6 +33,11 @@ return {
       filesystem = {
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
       },
       default_component_configs = {
         indent = {
