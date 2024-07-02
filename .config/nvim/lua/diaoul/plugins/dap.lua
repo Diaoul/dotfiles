52453314591dbc6704acbddccc716b4db66d74db
Antoine Bertin <antoine@bertin.io>
2024-07-02 11:19:54 +0200

    fix(nvim): dap can specify args as string for python

--- .config/nvim/lua/diaoul/plugins/dap.lua
+++ .config/nvim/lua/diaoul/plugins/dap.lua
@@ -29,6 +29,69 @@ return {
         dap.listeners.before.event_exited["dapui_config"] = function()
           dapui.close()
         end
+
+        -- string parser with quote escaping capabilities
+        -- inspired by http://lua-users.org/wiki/LpegRecipes
+        --- @param input string
+        --- @return table
+        local function split(input)
+          local lpeg = vim.lpeg
+
+          local P, S, C, Cc, Ct = lpeg.P, lpeg.S, lpeg.C, lpeg.Cc, lpeg.Ct
+
+          --- @param id string
+          --- @param patt vim.lpeg.Capture
+          --- @return vim.lpeg.Pattern
+          local function token(id, patt)
+            return Ct(Cc(id) * C(patt))
+          end
+
+          local single_quoted = P("'") * ((1 - S("'\r\n\f\\")) + (P("\\") * 1)) ^ 0 * "'"
+          local double_quoted = P('"') * ((1 - S('"\r\n\f\\')) + (P("\\") * 1)) ^ 0 * '"'
+
+          local whitespace = token("whitespace", S("\r\n\f\t ") ^ 1)
+          local word = token("word", (1 - S("' \r\n\f\t\"")) ^ 1)
+          local string = token("string", single_quoted + double_quoted)
+
+          local pattern = Ct((string + whitespace + word) ^ 0)
+
+          local t = {}
+          local tokens = lpeg.match(pattern, input)
+
+          -- somehow, this did not work out
+          if tokens == nil or type(tokens) == "integer" then
+            return t
+          end
+
+          for _, tok in ipairs(tokens) do
+            if tok[1] ~= "whitespace" then
+              if tok[1] == "string" then
+                -- cut off quotes and replace escaped quotes
+                table.insert(t, tok[2]:sub(2, -2):gsub("\\(['\"])", "%1"))
+              else
+                table.insert(t, tok[2])
+              end
+            end
+          end
+
+          return t
+        end
+
+        -- add debugpy capability to specify args as string
+        dap.listeners.on_config["debugpy"] = function(config)
+          -- copy the config and split the args if it is a string
+          local c = {}
+
+          for k, v in pairs(vim.deepcopy(config)) do
+            if k == "args" and type(v) == "string" then
+              c[k] = split(v)
+            else
+              c[k] = v
+            end
+          end
+
+          return c
+        end
       end,
     },
 
