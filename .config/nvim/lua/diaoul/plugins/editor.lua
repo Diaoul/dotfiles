d77a00a84eedd8d61775702265b426e4f61608e2
Antoine Bertin <antoine@bertin.io>
2024-07-10 22:02:46 +0200

    fix(nvim): neotree mappings for cwd

--- .config/nvim/lua/diaoul/plugins/editor.lua
+++ .config/nvim/lua/diaoul/plugins/editor.lua
@@ -11,7 +11,15 @@ return {
         end,
         desc = "Explorer NeoTree",
       },
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
       { "<leader>e", "<leader>fe", desc = "Explorer NeoTree", remap = true },
+      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
       {
         "<leader>ge",
         function()
