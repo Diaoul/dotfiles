ddb8436d3a4cd36320a99649bd61e26cc3c9fa9a
Antoine Bertin <antoine@bertin.io>
2024-07-10 21:46:21 +0200

    fix(nvim): notification sizes and timeout

--- .config/nvim/lua/diaoul/plugins/ui.lua
+++ .config/nvim/lua/diaoul/plugins/ui.lua
@@ -2,7 +2,18 @@ return {
   -- Better `vim.notify()`
   {
     "rcarriga/nvim-notify",
-    opts = {},
+    opts = {
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
+    },
     init = function()
       vim.notify = require("notify")
     end,
