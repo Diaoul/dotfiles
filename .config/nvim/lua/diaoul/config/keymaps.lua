fb351342efa606a59737f1747e453342dcec464e
Antoine Bertin <antoine@bertin.io>
2025-05-10 19:31:25 +0200

    fix(nvim): keymaps

--- .config/nvim/lua/diaoul/config/keymaps.lua
+++ .config/nvim/lua/diaoul/config/keymaps.lua
@@ -1,19 +1,21 @@
 -- based off LazyVim https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 local map = vim.keymap.set
 
--- Wrapped lines navigation
-map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+-- remove some default mappings
+vim.keymap.del("n", "grr")
+vim.keymap.del({ "n", "v" }, "gra")
+vim.keymap.del("n", "grn")
+vim.keymap.del("n", "gri")
+vim.keymap.del("n", "gO")
+vim.keymap.del("i", "<C-s>")
 
--- PUM navigation (disabled, not working as expected)
--- map("c", "<Esc>", "pumvisible() ? '<C-e>' : '<Esc>'", { expr = true })
--- map("c", "<CR>", "pumvisible() ? '<C-y>' : '<CR>'", { expr = true })
--- map("i", "<Down>", "pumvisible() ? '<C-n>' : '<Down>'", { expr = true })
--- map("i", "<Up>", "pumvisible() ? '<C-p>' : '<Up>'", { expr = true })
+-- better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
--- Window navigation
+-- window navigation
 map("n", "<C-h>", "<C-w>h", { desc = "Go To Left Window", remap = true })
 map("n", "<C-j>", "<C-w>j", { desc = "Go To Lower Window", remap = true })
 map("n", "<C-k>", "<C-w>k", { desc = "Go To Upper Window", remap = true })
@@ -23,7 +25,7 @@ map("n", "<C-Down>", "<C-w>j", { desc = "Go To Lower Window", remap = true })
 map("n", "<C-Up>", "<C-w>k", { desc = "Go To Upper Window", remap = true })
 map("n", "<C-Right>", "<C-w>l", { desc = "Go To Right Window", remap = true })
 
--- Window resize
+-- window resize
 map("n", "<A-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 map("n", "<A-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 map("n", "<A-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
@@ -33,7 +35,7 @@ map("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 map("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
--- Move lines
+-- move lines
 map("n", "<S-Down>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
 map("n", "<S-Up>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
 map("i", "<S-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
@@ -43,16 +45,23 @@ map("v", "<S-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 map("v", "<S-Down>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
 map("v", "<S-Up>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 
--- Buffer navigation
+-- buffers
 map("n", "[b", "<cmd>bprevious<cr>", { desc = "Previous Buffer" })
 map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch To Other Buffer" })
 map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch To Other Buffer" })
+map("n", "<leader>bd", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
+map("n", "<leader>bo", function()
+  Snacks.bufdelete.other()
+end, { desc = "Delete Other Buffers" })
+map("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
 
--- Clear search with <esc>
-map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape And Clear HLSearch" })
+-- clear search with <esc>
+map({ "i", "n", "s" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape And Clear hlsearch" })
 
--- Always search in the same direction
+-- always search in the same direction
 -- see https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
 map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
@@ -61,80 +70,82 @@ map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search R
 map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
 map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
 
--- Undo break-points
+-- undo break-points
 map("i", ",", ",<c-g>u")
 map("i", ".", ".<c-g>u")
 map("i", ";", ";<c-g>u")
 
--- Save file
+-- save file
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
 -- keywordprg
 map("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
 
--- Indenting preserves selection
+-- indenting preserves selection
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
--- Lazy
+-- commenting
+map("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Below" })
+map("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Above" })
+
+-- lazy
 map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
--- New file
+-- new file
 map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
 
--- Location list
-map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
+-- location list
+map("n", "<leader>xl", function()
+  local success, err = pcall(vim.fn.getloclist(0, { winid = 0 }).winid ~= 0 and vim.cmd.lclose or vim.cmd.lopen)
+  if not success and err then
+    vim.notify(err, vim.log.levels.ERROR)
+  end
+end, { desc = "Location List" })
 
--- Quickfix list
-map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
+-- quickfix list
+map("n", "<leader>xq", function()
+  local success, err = pcall(vim.fn.getqflist({ winid = 0 }).winid ~= 0 and vim.cmd.cclose or vim.cmd.copen)
+  if not success and err then
+    vim.notify(err, vim.log.levels.ERROR)
+  end
+end, { desc = "Quickfix List" })
 map("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
 map("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
 
--- formatting
--- in conform.nvim
--- map({ "n", "v" }, "<leader>cf", function()
---   Util.format({ force = true })
--- end, { desc = "Format" })
-
--- Diagnostic
-local diagnostic_goto = function(next, severity)
-  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-  severity = severity and vim.diagnostic.severity[severity] or nil
+-- diagnostic
+local diagnostic_jump = function(direction, severity)
+  local opts = { count = direction, wrap = true }
+  if severity then
+    opts.severity = vim.diagnostic.severity[severity]
+  end
   return function()
-    go({ severity = severity })
+    vim.diagnostic.jump(opts)
   end
 end
 map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
-map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
-map("n", "[d", diagnostic_goto(false), { desc = "Previous Diagnostic" })
-map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Previous Error" })
-map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Previous Warning" })
+map("n", "]d", diagnostic_jump(1), { desc = "Next Diagnostic" })
+map("n", "[d", diagnostic_jump(-1), { desc = "Previous Diagnostic" })
+map("n", "]e", diagnostic_jump(1, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_jump(-1, "ERROR"), { desc = "Previous Error" })
+map("n", "]w", diagnostic_jump(1, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_jump(-1, "WARN"), { desc = "Previous Warning" })
 
--- toggle options
--- map("n", "<leader>uf", function() Util.format.toggle() end, { desc = "Toggle auto format (global)" })
--- map("n", "<leader>uF", function() Util.format.toggle(true) end, { desc = "Toggle auto format (buffer)" })
-
--- Quit
+-- quit
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 
--- Highlights under cursor
+-- highlights under cursor
 map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
+map("n", "<leader>uI", function()
+  vim.treesitter.inspect_tree()
+  vim.api.nvim_input("I")
+end, { desc = "Inspect Tree" })
 
--- Terminal navigation
-map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
-map("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go To Left Window" })
-map("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go To Lower Window" })
-map("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go To Upper Window" })
-map("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go To Right Window" })
+-- terminal navigation
 map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
 
--- Window management
-map("n", "<leader>ww", "<C-W>p", { desc = "Other Window", remap = true })
+-- window management
 map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
-map("n", "<leader>w-", "<C-W>s", { desc = "Split Window Below", remap = true })
-map("n", "<leader>w|", "<C-W>v", { desc = "Split Window Right", remap = true })
 map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
 map("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
