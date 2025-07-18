ccfc184dc866e3ed34c2fc13c67987c34cd75ae6
Antoine Bertin <antoine@bertin.io>
2025-07-18 17:12:38 +0200

    ref: sessionoptions and persistence new keymap

--- .config/nvim/lua/diaoul/config/options.lua
+++ .config/nvim/lua/diaoul/config/options.lua
@@ -33,6 +33,7 @@ opt.pumblend = 10 -- Popup blend
 opt.pumheight = 10 -- Maximum number of entries in a popup
 opt.relativenumber = true -- Relative line numbers
 opt.scrolloff = 8 -- Lines of context
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" } -- Session persistence
 opt.shiftround = true -- Round indent
 opt.shiftwidth = 2 -- Size of an indent
 opt.shortmess:append({ W = true, I = true, c = true, C = true })
