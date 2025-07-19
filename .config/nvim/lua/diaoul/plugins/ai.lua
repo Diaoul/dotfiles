f6c04de584128be44ae43d2de554e0be4b70df7e
Antoine Bertin <antoine@bertin.io>
2025-07-19 09:10:45 +0200

    fix(nvim): avante configuration

--- .config/nvim/lua/diaoul/plugins/ai.lua
+++ .config/nvim/lua/diaoul/plugins/ai.lua
@@ -42,7 +42,6 @@ return {
   -- avante
   {
     "yetone/avante.nvim",
-    event = "VeryLazy",
     build = "make",
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -54,20 +53,33 @@ return {
       "MeanderingProgrammer/render-markdown.nvim",
       "HakonHarnes/img-clip.nvim",
     },
+    -- stylua: ignore
+    keys = {
+      { "<leader>aa", function() require("avante.api").ask() end, mode = { "n", "v" }, desc = "Ask" },
+      { "<leader>an", function() require("avante.api").ask({new_chat = true}) end, mode = { "n", "v" }, desc = "New Ask" },
+      { "<leader>ae", function() require("avante.api").edit() end, mode = { "v" }, desc = "Edit" },
+      { "<leader>aS", function() require("avante.api").stop() end, mode = { "n" }, desc = "Stop" },
+      { "<leader>ar", function() require("avante.api").refresh() end, mode = { "n" }, desc = "Refresh" },
+      { "<leader>a`", function() require("avante.api").focus() end, mode = { "n" }, desc = "Focus" },
+      { "<leader>at", function() require("avante.api").toggle() end, mode = { "n" }, desc = "Toggle" },
+      { "<leader>ad", function() require("avante.api").toggle.debug() end, mode = { "n" }, desc = "Toggle Debug" },
+      { "<leader>aH", function() require("avante.api").toggle.hint() end, mode = { "n" }, desc = "Toggle Hints" },
+      { "<leader>as", function() require("avante.api").toggle.suggestion() end, mode = { "n" }, desc = "Toggle Suggestions" },
+      { "<leader>aM", function() require("avante.api").select_model() end, mode = { "n" }, desc = "Select Model" },
+      { "<leader>ah", function() require("avante.api").select_history() end, mode = { "n" }, desc = "Select History" },
+      { "<leader>aB", function() require("avante.api").add_buffer_files() end, mode = { "n" }, desc = "Add All Open Buffers" },
+    },
     ---@module 'avante'
     ---@type avante.Config
     opts = {
       hints = { enabled = false },
       provider = "claude",
-      auto_suggestions_provider = nil,
       behaviour = {
-        auto_suggestions = false,
+        auto_set_keymaps = false,
       },
-      file_selector = {
-        provider = "snacks",
-        provider_opts = {},
-      },
+      file_selector = { provider = "snacks" },
       selector = { provider = "snacks" },
+      input = { provider = "snacks" },
       system_prompt = function()
         local hub = require("mcphub").get_hub_instance()
         return hub and hub:get_active_servers_prompt() or ""
@@ -82,7 +94,27 @@ return {
           make_slash_commands = true,
         },
       },
-      input = { provider = "snacks" },
+      mappings = {
+        submit = {
+          insert = "<C-CR>",
+        },
+        sidebar = {
+          close_from_input = { normal = "q" },
+          switch_windows = "`",
+          reverse_switch_windows = "<S-`>",
+        },
+        files = {
+          add_current = false,
+        },
+      },
+      windows = {
+        ask = {
+          start_insert = false,
+        },
+        edit = {
+          border = "rounded",
+        },
+      },
     },
   },
 }
